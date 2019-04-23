54f0b1336e19412e8990c74f9ecba2be765f60f3
Tim Bavaro <tbavaro@gmail.com>
2019-04-22 22:21:32 -0700

    make search a little more responsive by doing the work outside the normal render loop. but it's still not cancel-able or off the main thread so there's only so much it can do

--- src/ui-structure/SearchPopperContents.tsx
+++ src/ui-structure/SearchPopperContents.tsx
@@ -22,6 +22,8 @@ interface Props {
 
 interface State {
   maxHeightPx: number;
+  latestAnsweredQuery: string | null;
+  latestAnsweredResults: MyNodeDatum[];
 }
 
 const elementHelper = document.createElement("div");
@@ -41,7 +43,9 @@ function cleanLabel(label: string) {
 
 class SearchPopperContents extends React.PureComponent<Props, State> {
   public state: State = {
-    maxHeightPx: 100
+    maxHeightPx: 100,
+    latestAnsweredQuery: null,
+    latestAnsweredResults: []
   };
 
   public componentWillMount() {
@@ -62,8 +66,14 @@ class SearchPopperContents extends React.PureComponent<Props, State> {
   }
 
   public render() {
-    const { document } = this.props;
-    const results = document.nodeSearchHelper.search(this.props.query);
+    // TODO avoid having side-effects within render
+    let isLoading = false;
+    if (this.state.latestAnsweredQuery !== this.props.query) {
+      this.rescheduleSearchWorker();
+      isLoading = true;
+    }
+
+    const results = this.state.latestAnsweredResults;
 
     if (this.props.setFirstSearchResult) {
       this.props.setFirstSearchResult(results.length === 0 ? null : results[0]);
@@ -79,7 +89,7 @@ class SearchPopperContents extends React.PureComponent<Props, State> {
         {
           results.length === 0
             ? this.renderEmptyState()
-            : this.renderResults(results)
+            : this.renderResults(results, isLoading)
         }
       </List>
     );
@@ -89,22 +99,42 @@ class SearchPopperContents extends React.PureComponent<Props, State> {
     return "No results";
   }
 
-  private renderResults(results: MyNodeDatum[]) {
+  private renderResults(results: MyNodeDatum[], isLoading: boolean) {
     const limitedResults = results.slice(0, 50);
 
     return limitedResults.map(result => (
-      <MyResultListItem key={result.id} node={result} jumpToNode={this.props.actions.jumpToNode}/>
+      <MyResultListItem key={result.id} node={result} jumpToNode={this.props.actions.jumpToNode} disabled={isLoading}/>
     ));
   }
 
   private updateMaxHeight = () => {
     this.setState({ maxHeightPx: Math.floor((window.innerHeight - 48) * 0.8) });
   }
+
+  private pendingSearchTimeoutId: NodeJS.Timeout | null = null;
+  private rescheduleSearchWorker = () => {
+    if (this.pendingSearchTimeoutId !== null) {
+      clearTimeout(this.pendingSearchTimeoutId);
+      this.pendingSearchTimeoutId = null;
+    }
+    this.pendingSearchTimeoutId = setTimeout(this.doSearchWork, 10);
+  }
+  private doSearchWork = () => {
+    const query = this.props.query;
+    console.log("query", query);
+    const document = this.props.document;
+    const results = document.nodeSearchHelper.search(query);
+    this.setState({
+      latestAnsweredQuery: query,
+      latestAnsweredResults: results
+    });
+  }
 }
 
 interface MyResultListItemProps {
   node: MyNodeDatum;
   jumpToNode: (node: MyNodeDatum) => void;
+  disabled?: boolean;
 };
 
 class MyResultListItem extends React.PureComponent<MyResultListItemProps, {}> {
@@ -115,7 +145,7 @@ class MyResultListItem extends React.PureComponent<MyResultListItemProps, {}> {
   public render() {
     const { node } = this.props;
     return (
-      <ListItem button={true} onClick={this.handleClick}>
+      <ListItem button={true} onClick={this.handleClick} disabled={this.props.disabled}>
         <ListItemText primary={cleanLabel(node.label)}/>
       </ListItem>
     );
