a629fef1698987a63dc78990695ed535b8526b9d
Tim Bavaro <tbavaro@gmail.com>
2019-10-25 22:07:59 -0700

    support for production autograph via sheets integration

--- sheets-addon/src/AutographPopupWindow.ts
+++ sheets-addon/src/AutographPopupWindow.ts
@@ -1,9 +1,25 @@
-import { templateElement } from "@babel/types";
-
 const DEFAULT_WIDTH = 1280;
 const DEFAULT_HEIGHT = 800;
 const POPUP_TITLE = "Autograph";
 const POPUP_TEMPLATE_FILENAME = "AutographPopup.template.html";
+
+interface MyEnvConfig {
+  url: string;
+  targetOrigin: string;
+  popupTitle: string;
+}
+
+const ENV_CONFIG_PROD: MyEnvConfig = {
+  url: "https://tbavaro.github.io/autograph",
+  targetOrigin: "*",
+  popupTitle: POPUP_TITLE
+};
+
+const ENV_CONFIG_DEV: MyEnvConfig = {
+  url: "https://localhost:3001",
+  targetOrigin: "*",
+  popupTitle: `${POPUP_TITLE} (dev)`
+};
 
 export default class AutographPopupWindow {
   public display(attrs: {
@@ -11,13 +27,15 @@ export default class AutographPopupWindow {
     data: any,
     isDev?: boolean
   }) {
+    const env = (attrs.isDev ? ENV_CONFIG_DEV : ENV_CONFIG_PROD);
+
     const template = HtmlService.createTemplateFromFile(POPUP_TEMPLATE_FILENAME);
-    template.url = (attrs.isDev ? "https://localhost:3001/" : "https://tbavaro.github.io/autograph/") + "?embedded";
-    template.targetOrigin = (attrs.isDev ? "*" : "tbavaro.github.io");
+    template.url = `${env.url}?embedded`;
+    template.targetOrigin = env.targetOrigin;
     template.appData = JSON.stringify(attrs.data);
     const html = template.evaluate();
     html.setWidth(DEFAULT_WIDTH);
     html.setHeight(DEFAULT_HEIGHT);
-    attrs.ui.showModalDialog(html, POPUP_TITLE);
+    attrs.ui.showModalDialog(html, env.popupTitle);
   }
 }
--- sheets-addon/src/index.ts
+++ sheets-addon/src/index.ts
@@ -1,61 +1,16 @@
+import { PositionData } from "./generated/SharedTypes";
+
 import AutographManagedSheet from "./AutographManagedSheet";
 import AutographPopupWindow from "./AutographPopupWindow";
 import { registerGlobalFunction } from "./GoogleAppsHelpers";
-import SheetHelper from "./SheetHelper";
-import { PositionData } from "./generated/SharedTypes";
-import { valueIfUndefined } from "./util";
 
 global.onOpen = () => {
   const ui = SpreadsheetApp.getUi();
-  ui.createMenu("Autograph")
+  ui.createAddonMenu()
+    .addItem("View in Autograph...", viewInAutographProd)
     .addItem("View in Autograph... (dev)", viewInAutographDev)
-    // .addItem("Do test", doTest)
     .addToUi();
 };
-
-// const doTest = registerGlobalFunction(() => {
-//   const sheet = SpreadsheetApp.getActiveSheet();
-
-//   const managedSheet = new AutographManagedSheet(sheet);
-
-//   const sheetHelper = new SheetHelper(sheet);
-//   const values = sheetHelper.readColumnData({
-//     aValues: {
-//       header: "a",
-//       transform: SheetHelperTransforms.asString
-//     },
-//     bValues: {
-//       header: "b",
-//       transform: SheetHelperTransforms.asNumberOrUndefined
-//     }
-//   });
-  
-//   const ui = SpreadsheetApp.getUi();
-//   showPreformattedDialog(ui, JSON.stringify({
-//     autographConfig: managedSheet.loadAutographConfig(),
-//     values
-//   }, null, 2));
-
-//   managedSheet.updateAutographConfig(c => {
-//     c.lastModifiedDate = `${new Date()}`;
-//   });
-
-//   const numValues = Math.floor(Math.random() * 1000);
-//   const valuesToWrite = [];
-//   for (let i = 0; i < numValues; ++i) {
-//     valuesToWrite.push(`value#${i}`);
-//   }
-//   sheetHelper.writeColumnData([
-//     {
-//       header: "write1",
-//       values: [
-//         new Date(),
-//         `expect ${numValues} values`,
-//         ...valuesToWrite
-//       ]
-//     }
-//   ]);
-// });
 
 function viewInAutographImpl(isDev: boolean) {
   const sheet = SpreadsheetApp.getActiveSheet();
@@ -71,6 +26,7 @@ function viewInAutographImpl(isDev: boolean) {
 }
 
 const viewInAutographDev = registerGlobalFunction(() => viewInAutographImpl(/*isDev=*/true));
+const viewInAutographProd = registerGlobalFunction(() => viewInAutographImpl(/*isDev=*/false));
 
 global.autographSavePositions = (data: PositionData) => {
   Logger.log("writing values");
